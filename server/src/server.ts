fa4f4e37fba93c714f85eb22fae4b9afd5677250
Giorgi Kharbedia <giorgikharbedia6@gmail.com>
2025-11-04 13:30:59 +0400

    deleted mock files, added mongoose package, changed server.ts file to work with mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,51 @@
-import * as dotenv from "dotenv";
 import express from "express";
+import * as path from "path";
 import cors from "cors";
-import { connectToDatabase } from "./database";
-import { employeeRouter } from "./employee.routes";
-
-// Load environment variables from the .env file, where the ATLAS_URI is configured
+import http from "http";
+import mongoose from "mongoose";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+// import { router } from "@angular-monorepo/mea-lib/router";
+import * as dotenv from "dotenv";
 dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:4200",
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api", (req, res) => {
+  res.send({ message: "Hello API" });
+});
 
 const { ATLAS_URI } = process.env;
 
 if (!ATLAS_URI) {
-  console.error(
-    "No ATLAS_URI environment variable has been defined in config.env"
-  );
-  process.exit(1);
+  throw new Error("ATLAS_URI is not defined");
 }
 
-connectToDatabase(ATLAS_URI)
-  .then(() => {
-    const app = express();
-    app.use(cors());
-    app.get("/", (_req, res) => {
-      res.send({ message: "Welcome to the API" });
-    });
-    app.use("/employees", employeeRouter);
+mongoose.Promise = Promise;
+mongoose.connect(ATLAS_URI);
+mongoose.connection.on("error", (err: Error) => {
+  console.log(err);
+});
 
-    // start the Express server
-    app.listen(5200, () => {
-      console.log(`Server running at http://localhost:5200...`);
-    });
-  })
-  .catch((error) => console.error(error));
+// app.use("/api", router());
